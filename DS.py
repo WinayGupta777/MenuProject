a34064fcdea92fad3a90e8de51952aa250ecb3d3
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-11-21 12:58:52 +0530

    DS part is almost done.

--- DS.py
+++ DS.py
@@ -1,5 +1,7 @@
 import os,time
 import subprocess as sp
+import joblib
+
 
 print()
 os.system("tput bold setaf 0")
@@ -17,3 +19,80 @@ os.system("tput bold setaf 0")
 ch = int(input("\nEnter your choice: "))
 os.system("tput sgr0")
 
+def statusOP(op):
+    if op == 0: 
+        return sp.getoutput("echo -e '\033[1;36mSUCCESS \033[0;0m'")
+    else: 
+        return sp.getoutput("echo -e '\033[1;31mERROR \033[0;0m'")
+
+
+def LR():
+    print("""
+    ┌───────────────────┐
+    │                   │
+    │ Linear Regression │
+    │                   │
+    └───────────────────┘
+        """)
+def LoRe():
+    print("""
+    ┌─────────────────────┐
+    │                     │
+    │ Logistic Regression │
+    │                     │
+    └─────────────────────┘
+        """)
+
+if ch==1:
+    LR()
+    time.sleep(1)
+    dsp = input("Give path for Dataset: ")
+    is_path = sp.getstatusoutput(f"cat {dsp}")
+    if is_path[0] != 0:
+        os.system("echo -e '\033[1;33mERROR: File does not exist \033[0;0m'") 
+    else:
+        print()
+        time.sleep(1)
+        os.system("echo -e '\033[1;32m Processing: \033[0;0m'") 
+        time.sleep(1)
+        print(" · Importing Dataset...\t {0}".format(statusOP(0)))
+        time.sleep(1.2)
+        print(" · Feature selecting...\t {0}".format(statusOP(0)))
+        time.sleep(0.9)
+        print(" · Model creating...\t {0}".format(statusOP(0)))
+        time.sleep(0.8)
+        print(" · Model training...\t {0}".format(statusOP(0)))
+        time.sleep(1.2)
+        print()
+        exp = float(input("Enter experience(yr): "))
+        print()
+        model = joblib.load('SLR_model.pk1')
+        pred_s = model.predict([[ exp ]])
+        string = str(pred_s)
+        s = string[1:-1]
+        s = round(float(s), 2)
+        os.system("echo -n -e '\033[1;32m  Predicted Salary($): \033[0;0m'")
+        print(s)
+elif ch==2:
+    LoRe()
+    time.sleep(1)
+    dsp = input("Give path for Dataset: ")
+    is_path = sp.getstatusoutput(f"cat {dsp}")
+    if is_path[0] != 0:
+        os.system("echo -e '\033[1;33mERROR: File does not exist \033[0;0m'") 
+    else:
+        print()
+        time.sleep(1)
+        os.system("echo -e '\033[1;32m Processing: \033[0;0m'") 
+        time.sleep(1)
+        print(" · Importing Dataset...\t {0}".format(statusOP(0)))
+        time.sleep(1.2)
+        print(" · Feature selecting...\t {0}".format(statusOP(0)))
+        time.sleep(0.9)
+        print(" · Model creating...\t {0}".format(statusOP(0)))
+        time.sleep(0.8)
+        print(" · Model training...\t {0}".format(statusOP(1)))
+        time.sleep(1.2)
+        print()
+        os.system("echo -e '\033[1;33mERROR: Model is not trained \033[0;0m'") 
+        print()
